711c102653dffbd3fdba0796198f39662a87a9e8
Maksym Khrebtan <m.khrebtan21@gmail.com>
2025-08-01 21:28:46 +0300

    Refactor AddExpense and AddIncome methods
    - Removed initialization of local variables
    - Introduced private method `IsValidCurrency` for improved currency validation logic.

--- src/Domain/Entities/Account.cs
+++ src/Domain/Entities/Account.cs
@@ -29,45 +29,66 @@ public class Account : AggregateRoot
         return Result<Account>.Success(new Account(Guid.NewGuid(), initialBalance.Value));
     }
 
+    public static Result<Account> Create(Money initialBalance)
+    {
+        if (initialBalance is null)
+        {
+            return Result<Account>.Failure(new Error("Account.InitialBalanceNull", "Initial balance cannot be null."));
+        }
+
+        return Result<Account>.Success(new Account(Guid.NewGuid(), initialBalance));
+    }
+
     public Result<Transaction> AddExpense(Money amount, DateTime date, string category = Transaction.DefaultCategory, string description = Transaction.DefaultDescription)
     {
+        if(!IsValidCurrency(amount.Currency))
+        {
+            return Result<Transaction>.Failure(new Error("Account.InvalidCurrency", "The currency of the amount does not match the account's currency."));
+        }
+
         Result<Transaction> expenseResult = Expense.Create(amount, date, category, description);
         if (expenseResult.IsFailure)
         {
             return expenseResult;
         }
 
-        Transaction expense = expenseResult.Value;
-        Money expenseAmount = expense.Amount;
-        Result<Money> newBalanceResult = Money.Create(Balance.Value - expenseAmount.Value, Balance.Currency);
+        Result<Money> newBalanceResult = Money.Create(Balance.Value - expenseResult.Value.Amount.Value, Balance.Currency);
         if (newBalanceResult.IsFailure)
         {
             return Result<Transaction>.Failure(newBalanceResult.Error);
         }
 
-        _transactions.Add(expense);
+        _transactions.Add(expenseResult.Value);
         Balance = newBalanceResult.Value;
-        return Result<Transaction>.Success(expense);
+        return Result<Transaction>.Success(expenseResult.Value);
     }
 
     public Result<Transaction> AddIncome(Money amount, DateTime date, string category = Transaction.DefaultCategory, string description = Transaction.DefaultDescription)
     {
+        if (!IsValidCurrency(amount.Currency))
+        {
+            return Result<Transaction>.Failure(new Error("Account.InvalidCurrency", "The currency of the amount does not match the account's currency."));
+        }
+
         Result<Transaction> incomeResult = Income.Create(amount, date, category, description);
         if (incomeResult.IsFailure)
         {
             return incomeResult;
         }
 
-        Transaction income = incomeResult.Value;
-        Money incomeAmount = income.Amount;
-        Result<Money> newBalanceResult = Money.Create(Balance.Value + incomeAmount.Value, Balance.Currency);
+        Result<Money> newBalanceResult = Money.Create(Balance.Value + incomeResult.Value.Amount.Value, Balance.Currency);
         if (newBalanceResult.IsFailure)
         {
             return Result<Transaction>.Failure(newBalanceResult.Error);
         }
 
-        _transactions.Add(income);
+        _transactions.Add(incomeResult.Value);
         Balance = newBalanceResult.Value;
-        return Result<Transaction>.Success(income);
+        return Result<Transaction>.Success(incomeResult.Value);
+    }
+
+    private bool IsValidCurrency(string currency)
+    {
+        return Balance.Currency.Equals(currency, StringComparison.OrdinalIgnoreCase);
     }
 }
